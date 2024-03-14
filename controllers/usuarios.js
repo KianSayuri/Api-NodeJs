3285d123116f1a240f794a4d591d5918990d9dbc
KianSayuri <159494737+KianSayuri@users.noreply.github.com>
2024-03-14 20:42:08 -0300

    Aula 14/03

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -4,9 +4,22 @@ const db = require ('../database/connection');
 module.exports ={
     async listarMesas (request, response) {
         try{
-            return response.status(200).json({confirma: 'Listar Mesas'});
-        } catch (error){
-            return response.status(500).json({confirma: 'Erro', message: error});
-        },
-    };
+            return response.status(200).json({ 
+            //objeto é o parametro com chaves/cod
+            //json ele espera um objeto, objeto é oq tem chave/atributo e valor
+            //200 é um padrao da internet para definir se haverá sucesso ou não, msm coisa com 500
+            sucesso: true,
+            mensagem: 'Lista de usuários.',
+            dados: null
+            });
+        } 
+        
+        catch (error){
+            return response.status(500).json({
+                sucesso: false,
+                mensagem:`Erro na requisão. \n ${erro}`,
+                dados: null
+            });
+        }
+    }
 }
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require ('express');
-
+const router = require ('./routes/routes')
+const cors = require ('cors')
 const app = express();
 
-//const porta = process.env.PORT || 3333;
+app.use(cors());
+app.use(express.json());
+app.use(router);
 
 const porta = 3333;
+//const porta = process.env.PORT || 3333;
 
 app.listen (porta, () =>{
     //console.log('Servidor iniciado na porta' + porta);
