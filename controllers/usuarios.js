6314601670038e752d4de7aca831e8bb9e6ee22d
KianSayuri <159494737+KianSayuri@users.noreply.github.com>
2024-03-15 19:44:57 -0300

    Aula 14/03 - Ajuste

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,23 +1,23 @@
-const {json} = require ('express');
-const db = require ('../database/connection');
+const { json } = require('express');
+const db = require('../database/connection');
 
-module.exports ={
-    async listarMesas (request, response) {
-        try{
-            return response.status(200).json({ 
-            //objeto é o parametro com chaves/cod
-            //json ele espera um objeto, objeto é oq tem chave/atributo e valor
-            //200 é um padrao da internet para definir se haverá sucesso ou não, msm coisa com 500
-            sucesso: true,
-            mensagem: 'Lista de usuários.',
-            dados: null
+module.exports = {
+    async listarUsuarios(request, response) {
+        try {
+            return response.status(200).json({
+                //objeto é o parametro com chaves/cod
+                //json ele espera um objeto, objeto é oq tem chave/atributo e valor
+                //200 é um padrao da internet para definir se haverá sucesso ou não, msm coisa com 500
+                sucesso: true,
+                mensagem: 'Lista de usuários.',
+                dados: null
             });
-        } 
-        
-        catch (error){
+        }
+
+        catch (error) {
             return response.status(500).json({
                 sucesso: false,
-                mensagem:`Erro na requisão. \n ${erro}`,
+                mensagem: `Erro na requisão. \n ${error}`,
                 dados: null
             });
         }
